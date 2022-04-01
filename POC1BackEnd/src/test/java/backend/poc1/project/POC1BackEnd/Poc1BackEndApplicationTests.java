40081b2315c91113fdbcc6f42e237dfe12019a45
surendhirans <surendhiransam@gmail.com>
2022-04-01 15:35:48 +0530

    first commit

---
+++ POC1BackEnd/src/test/java/backend/poc1/project/POC1BackEnd/Poc1BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.poc1.project.POC1BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poc1BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Loan} from "./Tabulation";
+import Page from "./Page";
+import { Table } from "./Table";
+function App() {
+  return (
+    <>
+    <Table/>
+    </>
+    
+  );
+}
+
+export default App;
